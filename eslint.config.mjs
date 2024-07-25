1a3566eb00d516789b62521224071651c695ccbb
Alessandro <alessandro.siga@gmail.com>
2024-07-24 23:25:07 -0300

    fix: Adjusts eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,38 @@
-import { ESLint } from 'eslint';
-import { parser } from '@typescript-eslint/parser';
-import { plugin } from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import plugin from '@typescript-eslint/eslint-plugin';
 
-export default {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module'
+export default [
+  {
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': plugin
+    },
+    rules: {
+      'for-direction': 'error',
+      'getter-return': ['error', { allowImplicit: true }],
+      'no-async-promise-executor': 'error',
+      'no-await-in-loop': 'error',
+      '@typescript-eslint/adjacent-overload-signatures': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/ban-types': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'warn'
+    }
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint'],
-  rules: {}
-};
+  {
+    files: ['*.ts', '*.tsx'],
+    languageOptions: {
+      parser: parser,
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }]
+    }
+  }
+];
