5e4b44efeb44f9458de114fb667d502b4bce648d
Alessandro <alessandro.siga@gmail.com>
2025-01-26 12:50:48 -0300

    Description: This pull request adjusts openai methods for generate contents
    Changes included:
    - openai-service.ts
    - openai-detail.mock.ts
    - openai-resume.mock.ts
    - openai-test.mock.ts

--- src/services/openai-service.ts
+++ src/services/openai-service.ts
@@ -1,10 +1,15 @@
 import { OpenAI } from 'openai';
+import { mockDetail } from '../mocks/openai-detail.mock';
+import { mockResume } from '../mocks/openai-resume.mock';
+import { mockTest } from '../mocks/openai-test.mock';
 
 export default class OpenAIService {
   private api: OpenAI;
+  private environment: string;
 
   constructor() {
     this.api = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
+    this.environment = process.env.ENVIRONMENT!;
   }
 
   public async createAI(
@@ -12,46 +17,37 @@ export default class OpenAIService {
     field: string
   ): Promise<string | undefined> {
     try {
-      const userContent =
-        field === 'detail'
-          ? `Detalhe o seguinte tópico em 30 linhas: ${topic}`
-          : `Resuma o seguinte tópico em 10 linhas: ${topic}`;
+      if (this.environment.toLowerCase().trim() === 'development') {
+        return new Promise((response) => {
+          setTimeout(() => {
+            if (field === 'detail') {
+              response(mockDetail);
+            } else if (field === 'resume') {
+              response(mockResume);
+            } else {
+              response(mockTest);
+            }
+          }, 3000);
+        });
+      } else {
+        const userContent =
+          field === 'detail'
+            ? `Detalhe o seguinte tópico em 30 linhas: ${topic}`
+            : field === 'resume'
+            ? `Resuma o seguinte tópico em 10 linhas: ${topic}`
+            : `Avaliação com 10 questões alternativas e gabarito sobre o seguinte tópico: ${topic}`;
 
-      /*    
         const response = await this.api.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        temperature: 0.7,
-        max_tokens: 100,
-        messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content: userContent },
-        ],
-      });
-      */
-
-      const response = {
-        id: 'chatcmpl-abc123',
-        object: 'chat.completion',
-        created: 1677652283,
-        model: 'gpt-3.5-turbo-0613',
-        choices: [
-          {
-            index: 0,
-            message: {
-              role: 'assistant',
-              content: 'This is the generated text.',
-            },
-            finish_reason: 'stop',
-          },
-        ],
-        usage: {
-          prompt_tokens: 56,
-          completion_tokens: 31,
-          total_tokens: 87,
-        },
-      };
-
-      return response.choices[0]?.message?.content?.trim();
+          model: 'gpt-3.5-turbo',
+          temperature: 0.7,
+          max_tokens: 100,
+          messages: [
+            { role: 'system', content: 'You are a helpful assistant.' },
+            { role: 'user', content: userContent },
+          ],
+        });
+        return response.choices[0]?.message?.content?.trim();
+      }
     } catch (error) {
       return undefined;
     }
